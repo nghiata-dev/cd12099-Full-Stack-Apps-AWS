186962f32a9c8134b3cdbfff702e0662144a6aef
nghiata-dev <nghiata.dev@gmail.com>
2024-04-22 13:25:03 +0700

    project submission

--- project starter code/util/util.js
+++ project starter code/util/util.js
@@ -1,6 +1,10 @@
 import fs from "fs";
 import Jimp from "jimp";
+import { fileURLToPath } from "url";
+import { dirname, join, normalize } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,12 +13,15 @@ import Jimp from "jimp";
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
- export async function filterImageFromURL(inputURL) {
+export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      const response = await fetch(inputURL);
+      const rawImgFile = await response.arrayBuffer();
+      const photo = await Jimp.read(rawImgFile);
+      const outpath = `${join(__dirname)}${normalize(
+        "/tmp/filtered"
+      )}-${Math.floor(Math.random() * 2000)}.jpg`;
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
@@ -33,7 +40,7 @@ import Jimp from "jimp";
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
- export async function deleteLocalFiles(files) {
+export async function deleteLocalFiles(files) {
   for (let file of files) {
     fs.unlinkSync(file);
   }
